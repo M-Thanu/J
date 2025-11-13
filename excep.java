a44d4685fa1d9b4196175d26a07a23dc31742874
M-Thanu <mahanthanuja@gmail.com>
2025-11-13 22:51:44 +0530

    Exception Handling

---
+++ excep.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.*;
+class excep{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        /*System.out.println("Enter: ");
+        int a = in.nextInt();
+        System.out.println("Enter: ");
+        int b = in.nextInt();
+        try{
+            int result=a/b;
+            System.out.println("Results: "+result);
+        }
+        catch (ArithmeticException e){
+            System.out.println("Error: Division by zero is not allowed.");
+    
+        }
+        finally{
+            System.out.println("Program Completed");
+        }*/
+       
+
+       /*try{
+            System.out.println("Enter Integer: ");
+            int a = in.nextInt();
+            System.out.println("Integer: "+a);
+       }
+       catch (InputMismatchException e){
+            System.out.println("Input is not an integer");
+       }
+       finally{
+        System.out.println("Program Completed");
+       }*/
+
+      int age = in.nextInt();
+
+      if(age<18){
+        throw new ArithmeticException("You must be older than 18");
+      }else{
+        System.out.println("You can vote");
+      }
+    }
+}
